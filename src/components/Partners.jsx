8192e096f0a8f1301099e3b5303834def2deb11f
S NAVEEN KUMAR <navee1110@gmail.com>
2024-02-06 21:27:46 +0530

    styles updated

--- src/components/Partners.jsx
+++ src/components/Partners.jsx
@@ -2,31 +2,33 @@ import React from "react";
 
 export const Partners = () => {
   return (
-    <div className="  flex flex-col md:flex-row   justify-between items-center m-auto ml-[80px] mr-[80px] ">
+    <div className="flex flex-col md:flex-row justify-between items-center mx-auto md:mx-[80px]">
       <div>
-        <h1 className="font-semibold h-[52px] text-[#2A2A2A] text-[38px]">
+        <h1 className="font-semibold text-[38px] md:h-[52px] text-[#2A2A2A]">
           Our Partners
         </h1>
       </div>
       <div>
         <img src="hdfc.png" alt="" className="w-[157px] h-[47px]" />
-        <h1 className="font-semibold text-[24px] text-center m-4">HDFC Bank</h1>
+        <h1 className="font-semibold text-[24px] text-center md:text-left md:ml-4">
+          HDFC Bank
+        </h1>
       </div>
       <div>
-        <img src="icici.png" alt="" />
-        <h1 className="font-semibold text-[24px] text-center m-4">
+        <img src="icici.png" alt="" className="w-[157px] h-[47px]" />
+        <h1 className="font-semibold text-[24px] text-center md:text-left md:ml-4">
           ICICI Bank
         </h1>
       </div>
       <div>
-        <img src="icici.png" alt="" />
-        <h1 className="font-semibold text-[24px] text-center m-4">
+        <img src="icici.png" alt="" className="w-[157px] h-[47px]" />
+        <h1 className="font-semibold text-[24px] text-center md:text-left md:ml-4">
           ICICI Bank
         </h1>
       </div>
       <div>
-        <img src="bob.png" alt="" />
-        <h1 className="font-semibold text-[24px] text-center m-4">
+        <img src="bob.png" alt="" className="w-[157px] h-[47px]" />
+        <h1 className="font-semibold text-[24px] text-center md:text-left md:ml-4">
           Bank OF Baroda
         </h1>
       </div>
